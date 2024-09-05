74b0155441c854ce870930948243e7ea2b985351
austaras <austaras@outlook.com>
2024-09-06 07:56:07 +0800

    basic insert split

--- crates/data-structure/src/b_tree.rs
+++ crates/data-structure/src/b_tree.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fmt::Display, mem};
 
 use arrayvec::ArrayVec;
 
@@ -26,95 +26,78 @@ impl<T, const M: usize> BTree<T, M> {
     }
 }
 
-enum Search {
-    Less,
-    Greater,
-    Exact(usize),
-    Between(usize),
+impl<T: Display, const M: usize> Display for BTree<T, M> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
 }
 
 impl<T: Eq + Ord, const M: usize> BTree<T, M> {
-    fn binary_search(arr: &ArrayVec<T, M>, data: &T) -> Search {
-        if data < &arr[0] {
-            return Search::Less;
-        }
-
-        if data > arr.last().unwrap() {
-            return Search::Greater;
-        }
-
-        let mut curr = arr.len() / 2;
-
-        while curr < arr.len() {
-            match arr[curr].cmp(data) {
-                Ordering::Less => {
-                    if curr + 1 < arr.len() && &arr[curr + 1] > data {
-                        return Search::Between(curr);
-                    } else {
-                        curr = (arr.len() + curr) / 2
-                    }
-                }
-                Ordering::Equal => return Search::Exact(curr),
-                Ordering::Greater => {
-                    if curr >= 1 && &arr[curr - 1] < data {
-                        return Search::Between(curr - 1);
-                    } else {
-                        curr = curr / 2
-                    }
-                }
-            }
-        }
-
-        unreachable!()
-    }
-
-    fn search_child(child: &Node<T, M>, data: &T) -> bool {
+    fn search_child(child: &Node<T, M>, data: &T) -> Option<()> {
         match child {
-            Node::Internal(intl) => match Self::binary_search(&intl.data, data) {
-                Search::Less => Self::search_child(&intl.children[0], data),
-                Search::Greater => Self::search_child(intl.children.last().unwrap(), data),
-                Search::Exact(_) => true,
-                Search::Between(i) => Self::search_child(&intl.children[i + 1], data),
+            Node::Internal(intl) => match intl.data.binary_search(data) {
+                Err(i) => Self::search_child(&intl.children[i], data),
+                Ok(_) => Some(()),
             },
-            Node::Leaf(l) => {
-                if let Search::Exact(_) = Self::binary_search(l, data) {
-                    true
-                } else {
-                    false
-                }
-            }
+            Node::Leaf(l) => match l.binary_search(data) {
+                Ok(_) => Some(()),
+                Err(_) => None,
+            },
         }
     }
 
-    pub fn search(&self, data: &T) -> bool {
-        match Self::binary_search(&self.data, data) {
-            Search::Less => {
-                if self.children.len() > 0 {
-                    Self::search_child(&self.children[0], data)
+    pub fn search(&self, data: &T) -> Option<()> {
+        match self.data.binary_search(data) {
+            Err(i) => {
+                if self.children.len() > i {
+                    Self::search_child(&self.children[i], data)
                 } else {
-                    false
+                    None
                 }
             }
-            Search::Greater => {
-                if self.children.len() > self.data.len() + 1 {
-                    Self::search_child(self.children.last().unwrap(), data)
-                } else {
-                    false
-                }
-            }
-            Search::Exact(_) => true,
-            Search::Between(i) => {
-                if self.children.len() > i + 1 {
-                    Self::search_child(&self.children[i + 1], data)
-                } else {
-                    false
-                }
-            }
+            Ok(_) => Some(()),
         }
     }
 
-    pub fn insert(&mut self, data: T) -> bool {
-        false
+    pub fn insert(&mut self, data: T) -> Option<T> {
+        match self.data.binary_search(&data) {
+            Ok(i) => Some(mem::replace(&mut self.data[i], data)),
+            Err(i) => {
+                if self.data.is_full() {
+                    let mid = self.data.len() / 2;
+
+                    match i.cmp(&mid) {
+                        Ordering::Less => todo!(),
+                        Ordering::Equal => todo!(),
+                        Ordering::Greater => {
+                            let mid = mid + 1;
+                            let mut right = ArrayVec::new();
+
+                            right.extend(self.data.drain(mid + 1..));
+
+                            right.insert(i - mid - 1, data);
+
+                            let right = Box::new(Node::Leaf(right));
+
+                            let mid = self.data.pop().unwrap();
+
+                            let left = mem::replace(&mut self.data, ArrayVec::new());
+
+                            let left = Box::new(Node::Leaf(left));
+
+                            self.data.push(mid);
+
+                            self.children.push(left);
+                            self.children.push(right);
+                        }
+                    }
+                } else {
+                    self.data.insert(i, data);
+                }
+
+                None
+            }
+        }
     }
 
     pub fn delete(&mut self, data: &T) -> Option<T> {
@@ -125,4 +108,45 @@ impl<T: Eq + Ord, const M: usize> BTree<T, M> {
 #[cfg(test)]
 mod tests {
     use super::BTree;
+
+    impl<T: Eq + Ord, const M: usize> BTree<T, M> {
+        fn check(&self) {
+            for i in 0..self.data.len() - 1 {
+                assert!(self.data[i] < self.data[i + 1])
+            }
+        }
+    }
+
+    #[test]
+    fn basic() {
+        let mut tree = BTree::<_, 5>::new();
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(4);
+
+        tree.check();
+
+        assert_eq!(tree.search(&1), Some(()));
+        assert_eq!(tree.search(&2), Some(()));
+        assert_eq!(tree.search(&3), Some(()));
+        assert_eq!(tree.search(&4), Some(()));
+        assert_eq!(tree.search(&5), Some(()));
+    }
+
+    #[test]
+    fn basic_split() {
+        let mut tree = BTree::<_, 5>::new();
+        tree.insert(3);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(5);
+
+        tree.check();
+
+        assert_eq!(tree.search(&5), Some(()));
+    }
 }
